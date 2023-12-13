79bd02d9a9b38b1867d8003d6980a53be5f63ada
akashps <psakash2002.psa@gmail.com>
2023-12-13 13:03:02 +0530

    Basic sorts after implementing OOPS

--- 3_Basic_sorts.cpp
+++ 3_Basic_sorts.cpp
@@ -1,5 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std;
+class doSort{
+    public:
+        void bubble_sort(int a[], int n){
+            cout<<"Bubble Sort: ";
+                bool swapped;
+            for(int i=0;i<n-1;i++){
+                swapped=false;
+                for(int j=0;j<n-1-i;j++){
+                    if(a[j]>a[j+1]){
+                        int temp=a[j];
+                        a[j]=a[j+1];
+                        a[j+1]=temp;
+                        swapped=true;
+                    }
+                }
+                if(swapped==false)
+                    break;
+            }
+            }
+        void selection_sort(int a[], int n){
+            cout<<"Selection sort: ";
+            int min_index;
+            for(int i=0;i<n;i++){
+                min_index=i;
+                for(int j=i+1;j<n;j++){
+                    if(a[min_index]>a[j]){
+                        min_index=j;
+                    }
+                }
+                int temp=a[i];
+                a[i]=a[min_index];
+                a[min_index]=temp;
+            }
+        }
+        void insertion_sort(int a[], int n){
+            cout<<"Insertion Sort: ";
+            int temp,j;
+            for(int i=1;i<n;i++){
+                temp=a[i];
+                j=i-1;
+                while(j>=0 && a[j]>temp){
+                    a[j+1]=a[j];
+                    j--;
+                }
+                a[j+1]=temp;
+            }
+        }
+};
+
 int main(){
     int n;
     cout<<"Enter array size: ";
@@ -8,50 +57,10 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
-    /* ******Bubble sort*******
-    bool swapped;
-    for(int i=0;i<n-1;i++){
-        swapped=false;
-        for(int j=0;j<n-1-i;j++){
-            if(a[j]>a[j+1]){
-                int temp=a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
-                swapped=true;
-            }
-        }
-        if(swapped==false)
-            break;
-    }
-    */
-
-    /* *****Selection sort******
-    int min_index;
-    for(int i=0;i<n;i++){
-        min_index=i;
-        for(int j=i+1;j<n;j++){
-            if(a[min_index]>a[j]){
-                min_index=j;
-            }
-        }
-        int temp=a[i];
-        a[i]=a[min_index];
-        a[min_index]=temp;
-    }
-    */
-
-//  *******Insertion sort********
-    int temp,j;
-    for(int i=1;i<n;i++){
-        temp=a[i];
-        j=i-1;
-        while(j>=0 && a[j]>temp){
-            a[j+1]=a[j];
-            j--;
-        }
-        a[j+1]=temp;
-    }
-
+    doSort obj;
+    //obj.bubble_sort(a, n);
+    obj.selection_sort(a, n);
+    //obj.insertion_sort(a, n);
     for(int i=0;i<n;i++){
         cout<<a[i]<<" ";
     }
