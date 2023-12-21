29eb2f4cf273d34ff37e3fe5c3c31ac29ceeebe7
akashps <psakash2002.psa@gmail.com>
2023-12-21 18:23:16 +0530

    performed insertion operations

--- singlyLinkedList.cpp
+++ singlyLinkedList.cpp
@@ -29,4 +29,51 @@ int main(){
         }
     }
     display(head);
+    int choice, toContinue;
+    cout<<"\nWant to perform any operation?(Yes-1/No-0): ";
+    cin>>toContinue;
+    while(toContinue){
+        int pos, count=0;
+        cout<<"\nWhat operation you want to perform:\n1. Insertion at beginning\n2. Insertion at end \n3. Insertion at position\n4. Deletion at beginning\n5. Deletion at end\n6. Deletion at position\n7. Reverse the linked list\n";
+        cin>>choice;
+        node *newnode=new node;
+        switch(choice){
+        case 1:
+            cout<<"\nEnter element to be inserted: ";
+            cin>>newnode->data;
+            newnode->next=head;
+            head=newnode;
+            display(head);
+            break;
+        case 2:
+            cout<<"\nEnter element to be inserted: ";
+            cin>>newnode->data;
+            newnode->next=0;
+            temp=head;
+            while(temp->next!=0){
+                temp=temp->next;
+            }
+            temp->next=newnode;
+            display(head);
+            break;
+        case 3:
+            display(head);
+            cout<<"\nEnter position to be inserted: ";
+            cin>>pos;
+            cout<<"Enter number to be inserted: ";
+            cin>>newnode->data;
+            temp=head;
+            while(count<pos-2){
+                temp=temp->next;
+                count++;
+            }
+            newnode->next=temp->next;
+            temp->next=newnode;
+            display(head);
+            break;
+    }
+        cout<<"\nWant to perform any operation?(Yes-1/No-0): ";
+        cin>>toContinue;
+    }
+    
 }
